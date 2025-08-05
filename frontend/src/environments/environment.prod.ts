62771e1db0d5820aed0220f0fb71339c72a17bc1
Welton Felix <contas.weltonf@gmail.com>
2025-08-05 14:25:11 +0000

    add file replacements

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     // URL da API para quando estiver rodando LOCALMENTE (sem Docker)
-    apiUrl: 'http://localhost:3000/api'
+    apiUrl: 'https://iagenda.onrender.com/api'
   };
