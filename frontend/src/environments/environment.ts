17866db0f81630d8f7d9e727b9edae31186e4fce
Welton Felix <contas.weltonf@gmail.com>
2025-08-05 13:59:49 +0000

    add api key env var

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // URL da API para quando estiver rodando LOCALMENTE (sem Docker)
-    apiUrl: 'http://localhost:3000/api'
+    apiUrl: process.env.API_URL
   };
